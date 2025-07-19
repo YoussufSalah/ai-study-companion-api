cdbdc5e334bd75737788578a66d8ec8bb85e4364
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-19 16:21:22 +0300

    Added temp log message

--- controllers/gumroad.controller.js
+++ controllers/gumroad.controller.js
@@ -1,6 +1,7 @@
 import asyncWrapper from "../utils/asyncWrapper.js";
 
 const ping = asyncWrapper(async (req, res, next) => {
+    console.log(req.body);
     res.status(200).json({ status: "success" });
 });
 
