703b2c1b0eaf6196e83138f7820c2b65ebb652db
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-19 16:55:43 +0300

    Corrected testPing function call

--- routes/gumroad.route.js
+++ routes/gumroad.route.js
@@ -4,6 +4,6 @@ import gumroadController from "../controllers/gumroad.controller.js";
 const router = Router();
 
 router.post("/ping/", gumroadController.ping);
-router.post("/test-ping/", gumroadController.ping);
+router.post("/test-ping/", gumroadController.testPing);
 
 export default router;
