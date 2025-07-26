e5addaa15a406a00f8ec4a30b136844c55a99389
YoussufSalah <youssufsalahsaber@gmail.com>
2025-07-26 21:21:28 +0300

    Updated auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,34 +1,26 @@
 import CreateError from "../utils/createError.js";
-import supabase from "../config/supabaseClient.js";
 import createCrudHandlers from "../utils/crudFactory.js";
+import asyncWrapper from "../utils/asyncWrapper.js";
+import { jwtDecode } from "jwt-decode";
 
 const usersCrud = createCrudHandlers("users");
 
-const protect = async (req, res, next) => {
+const protect = asyncWrapper(async () => {
     const authHeader = req.headers.authorization;
     if (!authHeader?.startsWith("Bearer "))
         return next(new CreateError("No token provided", 401));
 
     const token = authHeader.split(" ")[1];
 
-    try {
-        const {
-            data: { user },
-            error,
-        } = await supabase.auth.getUser(token);
+    const decoded = jwtDecode(token);
 
-        if (error || !user)
-            return next(new CreateError("Invalid or expired token", 401));
+    const fullUser = await usersCrud.getOne(decoded.sub);
+    if (!fullUser)
+        return next(new CreateError("User not found in local DB", 404));
 
-        const fullUser = await usersCrud.getOne(user.id);
-        if (!fullUser)
-            return next(new CreateError("User not found in local DB", 404));
-
-        req.user = fullUser;
-        next();
-    } catch (err) {
-        return next(new CreateError("Auth verification failed", 401));
-    }
-};
+    req.user = fullUser;
+    console.log(fullUser);
+    next();
+});
 
 export default protect;
